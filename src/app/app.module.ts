f0488d7fcc759f4b783bde0fda6d9dcee053ef71
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-19 11:45:33 +0600

    Стилизация компонентов countries, country-items, country-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found.component';
+import { CountriesComponent } from './home/countries/countries.component';
+import { CountryItemComponent } from './home/countries/country-item/country-item.component';
+import { CountryDetailsComponent } from './home/country-details/country-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CountriesComponent,
+    CountryItemComponent,
+    CountryDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `
+    <div class="alert alert-danger">
+      Page is not found!
+    </div>
+  `,
+  styles: [`
+    .alert {
+      width: 700px;
+      display: block;
+      margin: 70px auto;
+    }
+  `],
+})
+export class NotFoundComponent { }
+
